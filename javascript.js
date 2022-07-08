2d1000d110d1b71d8b193dd679a2d8fe44370eaf
Daniel Chun <danielchun31@gmail.com>
2022-07-08 09:50:12 -0700

    If...else
    Issue with if...else sections needs work. Currently unable to prompt
    user until a correct input is received. May need to use just
    if...else rather than the else if.
    
    If that doesn't get any easier, maybe need to look into another
    approach. Maybe use a switch?

--- javascript.js
+++ javascript.js
@@ -1,23 +1,35 @@
 //computer's input for game. temporarily commented out until user side is ready.
-// function computerPlay() {
-//     return items[Math.floor(Math.random()*items.length)];
-// }
+function computerPlay() {
+    return items[Math.floor(Math.random()*items.length)];
+}
 
-// var items = ['rock', 'paper', 'scissors'];
+var items = ['rock', 'paper', 'scissors'];
 
-//user side needs to make sure only correct inputs are received (rock, paper, scissors)
-//if correct input = move on to game. if incorrect = prompt again until correct
+console.log(computerPlay(items));
+
+
+function promptUser() {
+    prompt ('Please check spelling: Rock? Paper? or Scissors?');
+}
+
+// user side needs to make sure only correct inputs are received (rock, paper, scissors)
+// if correct input = move on to game. if incorrect = prompt again until correct
+
 function playGame
 (playerSelection = prompt('Rock? Paper? or Scissors?'), computerSelection) {
-    let playerInput = playerSelection.toUpperCase();
-    console.log(playerInput);
-    if (playerInput == "ROCK", "PAPER", "SCISSORS") {
-        let computerPlay = computerSelection;
-        console.log(computerPlay);
+    
+    let playerInput = playerSelection.toLowerCase();   
+    
+    if (playerInput !== "rock" || playerInput !== "paper" || playerInput !== "scissors") {
+        promptUser();
+    } else if (playerInput === "rock" || playerInput === "paper" || playerInput === "scissors") {
+        console.log('user submitted correct input');
     } else {
-        prompt ("Please check spelling; Rock? Paper? or Scissors?")
-    }
+        console.log('wrong input');
+    } 
+console.log(playerInput);
 }
+
 
 playGame();
 
