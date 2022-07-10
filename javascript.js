bce436e134ed78ccda5f268b00f817c248b60db1
Daniel Chun <danielchun31@gmail.com>
2022-07-10 12:25:52 -0700

    computerPlay() function
    The playGame function is not functioning properly. Ties and bad
    inputs are not accurately caught anymore. This was caused when I
    tried listing game possibilities if player inputs "rock".
    
    Need to figure out why cpuInput and playerInput are not comparing
    themselves correctly.

--- javascript.js
+++ javascript.js
@@ -1,26 +1,40 @@
-//computer's input for game. temporarily commented out until user side is ready.
+//created cpuInput as a variable to hold the value of the computerPlay() function
 function computerPlay() {
-    return items[Math.floor(Math.random()*items.length)];
+    const items = ['rock', 'paper', 'scissors'];   
+    var cpuInput = [Math.floor(Math.random()*items.length)];
+    return cpuInput;
 }
 
-var items = ['rock', 'paper', 'scissors'];
+//bad inputs and ties are not accurately caught
+//TypeError at the end of line 10 - "computerPlay() is not a function"
+function playGame(playerSelection = prompt('Rock? Paper? or Scissors?'), computerPlay()) {
+    console.log(playerSelection);
+    console.log(computerPlay());
+    // if (playerSelection.toLowerCase() === computerPlay()) {
+    //     console.log('tie');
+    // } else if ((playerSelection.toLowerCase() === 'rock')|| (playerSelection.toLowerCase() === 'paper')|| 
+    // (playerSelection.toLowerCase() === 'scissors')) {
+    //     console.log('good input - move on to game');
+    // } else if ((playerSelection.toLowerCase() !== 'rock')|| (playerSelection.toLowerCase() !== 'paper')|| 
+    // (playerSelection.toLowerCase() !== 'scissors')) {
+    //     console.log('bad input = prompt user again');
+    //     alert('Please re-enter your choice.');
+    //     playGame();
+    // }    
+    const playerInput = playerSelection.toLowerCase();
 
-console.log(computerPlay(items) + ' <- comp input');
-
-//bad inputs recognized, then re-prompt.
-//ties are not accurately caught
-function playGame(playerSelection = prompt('Rock? Paper? or Scissors?'), computerSelection = computerPlay(items)) {
-    
-    if (playerSelection.toLowerCase() === computerPlay()) {
+    if (computerPlay() === 'rock' && playerInput === 'rock') {
         console.log('tie');
-    } else if (playerSelection.toLowerCase() === 'rock'|| playerSelection.toLowerCase() === 'paper'|| playerSelection.toLowerCase() === 'scissors') {
-        console.log('good input - move on to game');
-    } else if (playerSelection.toLowerCase() !== 'rock'|| playerSelection.toLowerCase() !== 'paper'|| playerSelection.toLowerCase() !== 'scissors') {
-        console.log('bad input = prompt user again');
-        alert('Please re-enter your choice.');
-        playGame();
-    }    
-}
+    } else if (computerPlay() === 'rock' && playerInput === 'scissors') {
+        console.log('CPU wins');
+    } else if (computerPlay() === 'rock' && playerInput === 'paper') {
+        console.log('player wins');
+    } else if (playerInput !== 'rock' || playerInput !== 'paper' || playerInput !== 'scissors') {
+        console.log('bad input');
+    } else {
+        console.log('what happened?');
+    }
 
+}
 
 playGame();
