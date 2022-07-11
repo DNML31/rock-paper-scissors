64db0435abebd16ac4d722ad9a2a126120e6dd22
Daniel Chun <danielchun31@gmail.com>
2022-07-10 21:11:54 -0700

    computerPlay() reworked
    This function was slightly reworked after trying to understanding how
    it works a little more.
    
    Changed playGame parameters to playerSelection and computerSelection.
    
    Encountering issues getting inaccurate 'tie' and 'bad input'.

--- javascript.js
+++ javascript.js
@@ -1,40 +1,29 @@
 //created cpuInput as a variable to hold the value of the computerPlay() function
-function computerPlay() {
-    const items = ['rock', 'paper', 'scissors'];   
-    var cpuInput = [Math.floor(Math.random()*items.length)];
-    return cpuInput;
+function computerPlay(items) {
+    // const items = ['rock', 'paper', 'scissors'];   
+    // var cpuInput = [Math.floor(Math.random()*items.length)];
+    // return cpuInput;
+    return items[Math.floor(Math.random() * items.length)];
 }
+var items = ['rock', 'paper', 'scissors'];
+console.log(computerPlay(items));
 
-//bad inputs and ties are not accurately caught
-//TypeError at the end of line 10 - "computerPlay() is not a function"
-function playGame(playerSelection = prompt('Rock? Paper? or Scissors?'), computerPlay()) {
+function playGame(playerSelection = prompt('Rock? Paper? or Scissors?'), computerSelection = computerPlay(items)) {
     console.log(playerSelection);
-    console.log(computerPlay());
-    // if (playerSelection.toLowerCase() === computerPlay()) {
-    //     console.log('tie');
-    // } else if ((playerSelection.toLowerCase() === 'rock')|| (playerSelection.toLowerCase() === 'paper')|| 
-    // (playerSelection.toLowerCase() === 'scissors')) {
-    //     console.log('good input - move on to game');
-    // } else if ((playerSelection.toLowerCase() !== 'rock')|| (playerSelection.toLowerCase() !== 'paper')|| 
-    // (playerSelection.toLowerCase() !== 'scissors')) {
-    //     console.log('bad input = prompt user again');
-    //     alert('Please re-enter your choice.');
-    //     playGame();
-    // }    
-    const playerInput = playerSelection.toLowerCase();
 
-    if (computerPlay() === 'rock' && playerInput === 'rock') {
+    //const playerInput = playerSelection.toLowerCase();
+
+    if (computerSelection === 'rock' && playerSelection === 'rock') {
         console.log('tie');
-    } else if (computerPlay() === 'rock' && playerInput === 'scissors') {
+    } else if (computerSelection === 'rock' && playerSelection === 'scissors') {
         console.log('CPU wins');
-    } else if (computerPlay() === 'rock' && playerInput === 'paper') {
+    } else if (computerSelection === 'rock' && playerSelection === 'paper') {
         console.log('player wins');
-    } else if (playerInput !== 'rock' || playerInput !== 'paper' || playerInput !== 'scissors') {
+    } else if (playerSelection !== 'rock' || playerSelection !== 'paper' || playerSelection !== 'scissors') {
         console.log('bad input');
     } else {
         console.log('what happened?');
     }
-
 }
 
 playGame();
