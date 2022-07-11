19a3f94ebdab97c6e7c26f56eed739aecbbbdaef
Daniel Chun <danielchun31@gmail.com>
2022-07-11 09:59:15 -0700

    Filling out playGame() function
    Currently the playGame() function will only console.log 'bad input'
    regardless of what player enters. All possible scenarios are written
    out as else...if statements.
    
    The computerPlay() function was reworked to include computerSelection
    variable. This variable is set as a parameter for playGame() but I'm
    not sure if it may be contributing to the issue of the 'bad input'.

--- javascript.js
+++ javascript.js
@@ -1,25 +1,40 @@
-//created cpuInput as a variable to hold the value of the computerPlay() function
+// function computerPlay(items) {
+//     return items[Math.floor(Math.random() * items.length)];
+// }
+
+// var items = ['rock', 'paper', 'scissors'];
+// let computerSelection = computerPlay(items);
+
+
 function computerPlay(items) {
-    // const items = ['rock', 'paper', 'scissors'];   
-    // var cpuInput = [Math.floor(Math.random()*items.length)];
-    // return cpuInput;
-    return items[Math.floor(Math.random() * items.length)];
+    let computerSelection = items[Math.floor(Math.random()*items.length)];
 }
 var items = ['rock', 'paper', 'scissors'];
-console.log(computerPlay(items));
 
-function playGame(playerSelection = prompt('Rock? Paper? or Scissors?'), computerSelection = computerPlay(items)) {
+
+//keeps getting 'bad input'
+function playGame(playerSelection = prompt('Rock? Paper? or Scissors?'), computerSelection) {
     console.log(playerSelection);
 
-    //const playerInput = playerSelection.toLowerCase();
-
-    if (computerSelection === 'rock' && playerSelection === 'rock') {
+    if (computerSelection === 'rock' && playerSelection.toLowerCase() === 'rock') {
         console.log('tie');
-    } else if (computerSelection === 'rock' && playerSelection === 'scissors') {
-        console.log('CPU wins');
-    } else if (computerSelection === 'rock' && playerSelection === 'paper') {
+    } else if (computerSelection === 'paper' && playerSelection.toLowerCase() === 'paper') {
+        console.log('tie');
+    } else if (computerSelection === 'scissors' && playerSelection.toLowerCase() === 'scissors') {
+        console.log('tie');
+    } else if (computerSelection === 'rock' && playerSelection.toLowerCase() === 'paper') {
         console.log('player wins');
-    } else if (playerSelection !== 'rock' || playerSelection !== 'paper' || playerSelection !== 'scissors') {
+    } else if (computerSelection === 'paper' && playerSelection.toLowerCase() === 'scissors') {
+        console.log('player wins');
+    } else if (computerSelection === 'scissors' && playerSelection.toLowerCase() === 'rock') {
+        console.log('player wins');
+    } else if (computerSelection === 'rock' && playerSelection.toLowerCase() === 'scissors') {
+        console.log('CPU wins');
+    } else if (computerSelection === 'paper' && playerSelection.toLowerCase() === 'rock') {
+        console.log('CPU wins');
+    } else if (computerSelection === 'scissors' && playerSelection.toLowerCase() === 'paper') {
+        console.log('CPU wins');
+    } else if (playerSelection.toLowerCase() != 'rock' || playerSelection.toLowerCase() != 'paper' || playerSelection.toLowerCase() != 'scissors') {
         console.log('bad input');
     } else {
         console.log('what happened?');
