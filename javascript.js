8e7cb8e94ac2c545c87080bf04186d43182130ae
Daniel Chun <danielchun31@gmail.com>
2022-07-12 13:17:56 -0700

    Score logging
    The playRound() function now has console.log to show the score after
    every round. Variables for playerScore and cpuScore were created to
    track the results of these rounds.
    
    In the game() function, after 5 rounds, the winner will be announced
    or a message will show that it is a tie.
    
    The HTML also has two lines of text explaining what will happen.

--- javascript.js
+++ javascript.js
@@ -1,20 +1,28 @@
 function playerWin() {
     alert('You win!');
+    console.log('PLAYER WIN');  
+    playerScore++;
 }
 
 function cpuWin() {
     alert('CPU win.');
+    console.log('CPU WIN')
+    cpuScore++;
 }
 
 function tieGame() {
     alert('Tiegame.');
+    console.log('TIE')
 }
-
 
 function computerPlay(items) {
     return computerSelection = items[Math.floor(Math.random()*items.length)];
 }
 var items = ['rock', 'paper', 'scissors'];
+
+//variables added to keep score
+let playerScore = 0;
+let cpuScore = 0;
 
 function playRound(playerSelection = prompt('Rock? Paper? or Scissors?'), computerSelection) {
     console.log(playerSelection);
@@ -23,13 +31,13 @@ function playRound(playerSelection = prompt('Rock? Paper? or Scissors?'), comput
     console.log(computerSelection);    
 
     if (computerSelection === 'rock' && playerSelection.toLowerCase() === 'rock') {
-        console.log('Tie.');
-        tieGame();
+        // console.log('Tie.');
+        // tieGame();
+        return tieGame();
+
     } else if (computerSelection === 'paper' && playerSelection.toLowerCase() === 'paper') {
-        console.log('Tie.');
         tieGame();
     } else if (computerSelection === 'scissors' && playerSelection.toLowerCase() === 'scissors') {
-        console.log('Tie.');
         tieGame();
     } else if (computerSelection === 'rock' && playerSelection.toLowerCase() === 'paper') {
         console.log('Paper beats rock. You win.');
@@ -54,17 +62,25 @@ function playRound(playerSelection = prompt('Rock? Paper? or Scissors?'), comput
         alert('Please check spelling and re-enter.');
         playRound();
         return;    
-     }
+    }
+//this reports the score after every round
+    console.log("Player: " + playerScore);
+    console.log("CPU: " + cpuScore);
+
 }
 //playRound();
 
 function game() {
-//executes playRound() five times
     for (let i = 0; i < 5; i++) {
         playRound();
     }
-    // console.count(player);
-    // console.count(cpu);
-    // console.count(tie);
+
+    if (playerScore > cpuScore) {
+        alert('You beat the CPU.');
+    } else if (cpuScore > playerScore) {
+        alert('The CPU beat you.');
+    } else if (playerScore == cpuScore) {
+        alert('It\'s a tie. Play another set.');
+    }
 }
 game();
