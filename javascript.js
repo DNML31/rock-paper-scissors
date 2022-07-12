524c16d5c5cc1fb4901f2a3adfb8f34eda365f18
Daniel Chun <danielchun31@gmail.com>
2022-07-12 10:30:42 -0700

    Function game() continued...
    The game() function executes playRound() five times successfully. It
    catches bad inputs and does not count it as one of the five rounds.
    
    The playRound() function was changed to include a function after
    every outcome - this was to try to use console.count to log how many
    times a certain function (win announcement) is called. I tried using
    this as a way to count score but it is not working properly.

--- javascript.js
+++ javascript.js
@@ -1,3 +1,15 @@
+function playerWin() {
+    alert('You win!');
+}
+
+function cpuWin() {
+    alert('CPU win.');
+}
+
+function tieGame() {
+    alert('Tiegame.');
+}
+
 
 function computerPlay(items) {
     return computerSelection = items[Math.floor(Math.random()*items.length)];
@@ -11,29 +23,32 @@ function playRound(playerSelection = prompt('Rock? Paper? or Scissors?'), comput
     console.log(computerSelection);    
 
     if (computerSelection === 'rock' && playerSelection.toLowerCase() === 'rock') {
-        console.log('Tiegame.');
+        console.log('Tie.');
+        tieGame();
     } else if (computerSelection === 'paper' && playerSelection.toLowerCase() === 'paper') {
-        console.log('Tiegame.');
+        console.log('Tie.');
+        tieGame();
     } else if (computerSelection === 'scissors' && playerSelection.toLowerCase() === 'scissors') {
-        console.log('Tiegame.');
+        console.log('Tie.');
+        tieGame();
     } else if (computerSelection === 'rock' && playerSelection.toLowerCase() === 'paper') {
-        console.log('Paper beats rock.');
-        console.log('Player wins.');
+        console.log('Paper beats rock. You win.');
+        playerWin();
     } else if (computerSelection === 'paper' && playerSelection.toLowerCase() === 'scissors') {
-        console.log('Scissors beats paper.');
-        console.log('Player wins.');
+        console.log('Scissors beats paper. You win.');
+        playerWin();
     } else if (computerSelection === 'scissors' && playerSelection.toLowerCase() === 'rock') {
-        console.log('Rock beats scissors.');
-        console.log('Player wins.');
+        console.log('Rock beats scissors. You win.');
+        playerWin();
     } else if (computerSelection === 'rock' && playerSelection.toLowerCase() === 'scissors') {
-        console.log('Rock beats scissors.');
-        console.log('CPU wins.');
+        console.log('Rock beats scissors. CPU wins.');
+        cpuWin();
     } else if (computerSelection === 'paper' && playerSelection.toLowerCase() === 'rock') {
-        console.log('Paper beats rock.');
-        console.log('CPU wins.');
+        console.log('Paper beats rock. CPU wins.');
+        cpuWin();
     } else if (computerSelection === 'scissors' && playerSelection.toLowerCase() === 'paper') {
-        console.log('Scissors beats paper.');
-        console.log('CPU wins.');
+        console.log('Scissors beats paper. CPU wins');
+        cpuWin();
     } else if (playerSelection.toLowerCase() !== 'rock' && playerSelection.toLowerCase() !== 'paper' 
         && playerSelection.toLowerCase() !== 'scissors') {
         alert('Please check spelling and re-enter.');
@@ -41,14 +56,15 @@ function playRound(playerSelection = prompt('Rock? Paper? or Scissors?'), comput
         return;    
      }
 }
-playRound();
+//playRound();
 
 function game() {
-//need to utilize a loop to make playRound() execute 5 times and keep score
-    // playRound();
-    // playRound();
-    // playRound();
-
+//executes playRound() five times
+    for (let i = 0; i < 5; i++) {
+        playRound();
+    }
+    // console.count(player);
+    // console.count(cpu);
+    // console.count(tie);
 }
-
-//game();
+game();
