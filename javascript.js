c8f3cf40d826db4d691531233df11723b4e951d9
Daniel Chun <danielchun31@gmail.com>
2022-07-28 13:50:07 -0700

    Add buttons and buttonPush
    Player can now make a selection with buttons instead of typing into a
    prompt. Each button assigns playerSelection as the corresponding choice
    and runs the playRound function.

--- javascript.js
+++ javascript.js
@@ -1,3 +1,4 @@
+// game result alerts
 function playerWin() {
     alert('You win!');
     console.log('PLAYER WIN');  
@@ -15,24 +16,28 @@ function tieGame() {
     console.log('TIE')
 }
 
+//score count
+function scoreCount() {
+    console.log ("Player: " + playerScore);
+    console.log ("CPU: " + cpuScore);
+}
+
+// computer's choice
 function computerPlay(items) {
     return computerSelection = items[Math.floor(Math.random()*items.length)];
 }
 var items = ['rock', 'paper', 'scissors'];
 
-//variables added to keep score
 let playerScore = 0;
 let cpuScore = 0;
 
-function playRound(playerSelection = prompt('Rock? Paper? or Scissors?'), computerSelection) {
-    console.log(playerSelection);
+function playRound(playerSelection = prompt ("rock? paper? scissors?"), computerSelection) {
+    computerSelection = computerPlay(items); 
 
-    computerSelection = computerPlay(items);
-    console.log(computerSelection);    
+    // console.log(playerSelection);
+    // console.log(computerSelection);
 
     if (computerSelection === 'rock' && playerSelection.toLowerCase() === 'rock') {
-        // console.log('Tie.');
-        // tieGame();
         return tieGame();
 
     } else if (computerSelection === 'paper' && playerSelection.toLowerCase() === 'paper') {
@@ -63,11 +68,32 @@ function playRound(playerSelection = prompt('Rock? Paper? or Scissors?'), comput
         playRound();
         return;    
     }
-//this reports the score after every round
-    console.log("Player: " + playerScore);
-    console.log("CPU: " + cpuScore);
 
+    scoreCount();
 }
+
+const buttonPushRock = document.querySelector("#rock");
+buttonPushRock.addEventListener('click', ()=> {
+    playRound('rock', computerPlay(items));
+});
+
+const buttonPushPaper = document.querySelector('#paper');
+buttonPushPaper.addEventListener('click', ()=> {
+    playRound('paper',computerPlay(items));
+});
+
+const buttonPushScissors = document.querySelector('#scissors');
+buttonPushScissors.addEventListener('click', ()=> {
+    playRound('scissors',computerPlay(items));
+});
+
+
+const scoreBoard = document.querySelector('#score');
+let score = document.createElement('div');
+scoreBoard.append(scoreCount());
+
+
+
 
 // function game() {
 //     for (let i = 0; i < 5; i++) {
@@ -84,4 +110,5 @@ function playRound(playerSelection = prompt('Rock? Paper? or Scissors?'), comput
 // }
 // game();
 
-playRound();
+
+
